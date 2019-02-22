3fd7bf0412d5b4a82347f43ec5c110785b140b87
Andy Ylitalo <13158190+andylitalo@users.noreply.github.com>
2019-02-21 22:24:14 -0800

    Created Methods.py

---
+++ Methods.py
@@ -0,0 +1,9 @@
+"""
+List of methods for use in Project 2. Make sure to pull this when it is updated so your code
+is compatible with the current version.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
