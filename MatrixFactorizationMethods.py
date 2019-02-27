2d8b55bdc3780b44557384af8caafb249aba0902
Andy Ylitalo <13158190+andylitalo@users.noreply.github.com>
2019-02-26 21:10:58 -0800

    Now saves U and V as they are defined in guidebook

--- MatrixFactorizationMethods.py
+++ MatrixFactorizationMethods.py
@@ -134,9 +134,9 @@ if __name__=='__main__':
     
     # save results
     params = {}
-    params['method 1'] = [U1, V1, RMSE1]
-    params['method 2'] = [U2, V2, a2, b2, RMSE2]
-    params['method 3'] = [U3, V3, a3, b3, RMSE3]
+    params['method 1'] = [np.transpose(U1), np.transpose(V1), RMSE1]
+    params['method 2'] = [np.transpose(U2), np.transpose(V2), a2, b2, RMSE2]
+    params['method 3'] = [np.transpose(U3), np.transpose(V3), a3, b3, RMSE3]
     
     with open('data/matrix_factorization.pkl', 'wb') as f:
         pkl.dump(params, f)
