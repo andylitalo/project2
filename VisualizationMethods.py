e35baf0bee6e5b58fe3cbfd4e1d40b70f0d1dcbc
Andy Ylitalo <13158190+andylitalo@users.noreply.github.com>
2019-02-28 00:07:32 -0800

    Rank genres by variance feature added

--- VisualizationMethods.py
+++ VisualizationMethods.py
@@ -7,6 +7,7 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
 
 
 def load_projections(filepath='data/projection_data.pkl'):
@@ -155,9 +156,107 @@ def plot_genres_three_methods(movie_data, data_all, genre_list, x_lim=[-5,5], y_
         ax.set_title(title)
         ax.set_xlim(x_lim)
         ax.set_ylim(y_lim)
+        plt.legend(loc="best")
     
+def title_genres(ind, headers):
+    """
+    Returns string of movie title followed by genres.
+    """
+    movie_str = movie_data['Movie Title'][ind]
+    for i in range(2,len(headers)-1):
+        genre = headers[i]
+        if movie_data[genre][ind]:
+            movie_str += ", " + genre
+            
+    return movie_str
     
+def print_most_extreme(movie_data, data_all, num=5):
+    """
+    Prints the names of the movies with the most extreme values of each coordinate.
+    """
+    headers = list(movie_data)
+    for i in range(len(data_all)):
+        print("")
+        print("Method %i" %(i+1))
+        # select data for given method
+        data = data_all[i]
+
+        # greatest positive x value
+        print("greatest positive x")
+        sorting_inds_x = np.argsort(data[0,:])
+        inds_x_pos = sorting_inds_x[-num:]
+        for j in inds_x_pos:
+            print(title_genres(j, headers))
+        
+        # smallest negative x value
+        print("lowest negative x")
+        inds_x_neg = sorting_inds_x[:num]
+        for j in inds_x_neg:
+            print(title_genres(j, headers))
+            
+        # greatest positive y value
+        print("greatest positive y")
+        sorting_inds_y = np.argsort(data[1,:])
+        inds_y_pos = sorting_inds_y[-num:]
+        for j in inds_y_pos:
+            print(title_genres(j, headers))
+            
+            
+        # lowest negative y value
+        print("lowest negative y")
+        inds_y_neg = sorting_inds_y[:num]
+        for j in inds_y_neg:
+            print(title_genres(j, headers))
+            
+        # closest to 0
+        print("closest to 0")
+        sorting_inds_radii = np.argsort(np.sqrt((data[0,:])**2+(data[1,:])**2))
+        inds_near_zero = sorting_inds_radii[:num]
+        for j in inds_near_zero:
+            print(title_genres(j, headers))
+            
+        # farthest from 0
+        print("farthest from 0")
+        inds_far_zero = sorting_inds_radii[-num:]
+        for j in inds_far_zero:
+            print(title_genres(j, headers))
+        
+        
+def order_genres_by_variance(movie_data, data_all):
+    """
+    Returns list of genres in order of decreasing variance for each method.
+    """
+    # get list of genres
+    genre_list = list(movie_data)[2:-1]
+    # initialize result, list of lists, each list is for one method
+    result = []
     
+    # loop through each method
+    for data in data_all:
+        # array of variances
+        var_arr = np.zeros([len(genre_list)])
+        for i in range(len(genre_list)):
+            genre = genre_list
+            inds = get_movies_from_genre(movie_data, genre)
+            proj = get_proj(inds, data)
+            (xc, yc) = np.mean(proj, axis=0)
+#            
+#            x = proj[:,0]
+#            y = proj[:,1]
+#            r = np.sqrt((x-xc)**2 + (y-yc)**2)
+#            var_arr[i] = np.mean(r)
+            
+            # how best to estimate variance?
+            pca = PCA(n_components=2)
+            pca.fit(proj)
+            var_arr[i] = pca.singular_values_[0]
+        
+        # sort by variance
+        inds = np.argsort(var_arr)[::-1]
+        sorted_genre_list = [genre_list[ind] for ind in inds]
+        result+= [sorted_genre_list]      
+           
+    return result
 # main function for running methods
 if __name__=='__main__':
     # load projected data (2 x M and 2 x N)
@@ -166,26 +265,41 @@ if __name__=='__main__':
     # load movies and ratings
     movie_data, rating_data = load_data()
     
-    # plot top 10 movies (ratings)
-    inds_best_ten = best_ten_movies(movie_data, rating_data)
-    best_ten_proj_1 = get_proj(inds_best_ten, movies_1)
-    plot_movies(best_ten_proj_1, title='top 10 - method 1')
+    data_all = [movies_1, movies_2, movies_3]
+    genre_lists = order_genres_by_variance(movie_data, data_all)
     
-    # plot top 10 movies (ratings) - method 2
-    inds_best_ten = best_ten_movies(movie_data, rating_data)
-    best_ten_proj_2 = get_proj(inds_best_ten, movies_2)
-    plot_movies(best_ten_proj_2, title='top 10 - method 2')
-    
-    # plot top 10 movies (ratings) - method 3
-    best_ten_proj_3 = get_proj(inds_best_ten, movies_3)
-    plot_movies(best_ten_proj_3, title='top 10 - method 3')
-    
-    # plot most popular 10 movies (popularity)
-    inds_pop_ten = pop_ten_movies(rating_data)
-    pop_ten_proj_1 = get_proj(inds_pop_ten, movies_1)
-    plot_movies(pop_ten_proj_1)
-
-    movies_all = [movies_1, movies_2, movies_3]
-    # compare genres
-    plot_genres_three_methods(movie_data, movies_all, ["Musical", "Horror",
-                                                       "Documentary", "Western"])
+    for i in range(len(genre_lists[1])):
+        genre = [genre_lists[1][i]]
+        plot_genres_three_methods(movie_data, [data_all[1]], genre)
+#    # plot top 10 movies (ratings)
+#    inds_best_ten = best_ten_movies(movie_data, rating_data)
+#    best_ten_proj_1 = get_proj(inds_best_ten, movies_1)
+#    plot_movies(best_ten_proj_1, title='top 10 - method 1')
+#    
+#    # plot top 10 movies (ratings) - method 2
+#    inds_best_ten = best_ten_movies(movie_data, rating_data)
+#    best_ten_proj_2 = get_proj(inds_best_ten, movies_2)
+#    plot_movies(best_ten_proj_2, title='top 10 - method 2')
+#    
+#    # plot top 10 movies (ratings) - method 3
+#    best_ten_proj_3 = get_proj(inds_best_ten, movies_3)
+#    plot_movies(best_ten_proj_3, title='top 10 - method 3')
+#    
+#    # plot most popular 10 movies (popularity)
+#    inds_pop_ten = pop_ten_movies(rating_data)    
+#
+#    pop_ten_proj_1 = get_proj(inds_pop_ten, movies_1)
+#    plot_movies(pop_ten_proj_1)
+#
+#    movies_all = [movies_1, movies_2, movies_3]
+#    # compare genres
+#    plot_genres_three_methods(movie_data, movies_all, ["Musical", "Horror",
+#                                                       "Childrens", "Western",
+#                                                       "Animation"])
+#    
+#    # compare genres
+#    plot_genres_three_methods(movie_data, movies_all, ["Drama", "Comedy",
+#                                                       "Childrens"])
+#    
+#    # print names of movies with most extreme values
+#    print_most_extreme(movie_data, movies_all)
