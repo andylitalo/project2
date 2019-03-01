3d43a12a78ce2b8879d1861c87ee5e00039bfd9d
Andy Ylitalo <13158190+andylitalo@users.noreply.github.com>
2019-02-28 17:15:56 -0800

    Delete Methods.py

--- Methods.py
+++
@@ -1,9 +0,0 @@
-"""
-List of methods for use in Project 2. Make sure to pull this when it is updated so your code
-is compatible with the current version.
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
