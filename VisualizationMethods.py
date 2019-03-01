facc8a49f386dc48e5689f98490d9d0983d1ea85
Andy Ylitalo <13158190+andylitalo@users.noreply.github.com>
2019-02-28 17:03:23 -0800

    Update VisualizationMethods.py

--- VisualizationMethods.py
+++ VisualizationMethods.py
@@ -6,8 +6,8 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
-
+from matplotlib.patches import Ellipse
+from matplotlib import cm
 
 def load_projections(filepath='data/projection_data.pkl'):
     """
@@ -93,12 +93,10 @@ def get_proj(inds, proj):
     """
     return np.array([proj[:,i] for i in range(len(proj[0,:])) if i in inds])
     
-def plot_movies(data, movie_data, title='', indices='',x_lim=[-5,5], y_lim=[-5,5]):
+def plot_movies(data, title='', x_lim=[-5,5], y_lim=[-5,5]):
     """
-    Plots labeled movie projections in standard format for consistency.
+    Plots movie projections in standard format for consistency.
     """
-    titles = movie_data['Movie Title'][indices]
-    titles = list(titles)
     x = data[:,0]
     y = data[:,1]
     fig = plt.figure()
@@ -107,8 +105,6 @@ def plot_movies(data, movie_data, title='', indices='',x_lim=[-5,5], y_lim=[-5,5
     ax.set_title(title)
     ax.set_xlim(x_lim)
     ax.set_ylim(y_lim)
-    for txt in range(len(x)):
-        ax.annotate(titles[txt],((x[txt],y[txt])))
     
 def plot_movies_three_methods(movie_data, data_all, genre, title_prefix='', x_lim=[-5,5], 
                               y_lim=[-5,5]):
@@ -122,47 +118,79 @@ def plot_movies_three_methods(movie_data, data_all, genre, title_prefix='', x_li
     
     # plot movies - method 1
     proj_1 = get_proj(inds, data_1)
-    plot_movies(proj_1, movie_data, indices = inds, title=title_prefix + "method 1")
-    
+    plot_movies(proj_1, title=title_prefix + "method 1")
 
     # plot movies - method 2
     proj_2 = get_proj(inds, data_2)
-    plot_movies(proj_2, movie_data, indices = inds, title=title_prefix + "method 2")
+    plot_movies(proj_2, title=title_prefix + "method 2")
     
     # plot movies - method 3
     proj_3 = get_proj(inds, data_3)
-    plot_movies(proj_3, movie_data, indices = inds, title=title_prefix + "method 3")
+    plot_movies(proj_3, title=title_prefix + "method 3")
  
-    
-def plot_genres_three_methods(movie_data, data_all, genre_list, x_lim=[-5,5], y_lim=[-5,5]):
+  
+
+def plot_genres(movie_data, data, genre_list, method_num, x_lim=[-3,3], y_lim=[-3,3],
+                plot_cov=False, plot_ellipse=False, save_plots=False, save_folder='figures/', 
+                ext='.png'):
     """
-    Plots movies of different genres in different colors for all three methods.
+    Plots movies of all genres on different plots for different methods.
     """
-    
-    for i in range(len(data_all)):
-        data = data_all[i]
+       
+    # plot movies from each genre in a different color
+    for j in range(len(genre_list)):
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        # plot movies from each genre in a different color
-        for j in range(len(genre_list)):
-            genre = genre_list[j]
-            # get indices of movies in genre
-            inds = get_movies_from_genre(movie_data, genre)
-            proj = get_proj(inds, data)
-            x = proj[:,0]
-            y = proj[:,1]
-            ax.plot(x, y, 'o', label=genre)
-        title = "Method " + str(i+1) + " - "
-        for i in range(len(genre_list)-1):
-            title += genre_list[i]
-            if i < len(genre_list):
-                title += ", "
-        ax.set_title(title)
-        ax.set_xlim(x_lim)
-        ax.set_ylim(y_lim)
-        plt.legend(loc="best")
+        genre = genre_list[j]
+        # get indices of movies in genre
+        inds = get_movies_from_genre(movie_data, genre)
+        proj = get_proj(inds, data)
+        x = proj[:,0]
+        y = proj[:,1]
+        scatter, = ax.plot(x, y, 'o', label=genre, zorder=1)
+        if plot_cov:
+            xc, yc, lambda_, eig_vecs, var = get_cov_eig(movie_data,data, genre)
+            scatter.set_label(genre + ", %.3f" %var)
+            # first eigenvector
+            ax.arrow(xc, yc, lambda_[0]*eig_vecs[:,0][0], 
+                     lambda_[0]*eig_vecs[:,0][1], width=0.05, color='k',
+                     zorder=2)
+            # second eigenvector
+            ax.arrow(xc, yc, lambda_[1]*eig_vecs[:,1][0], 
+                     lambda_[1]*eig_vecs[:,1][1], width=0.05, color='k',
+                     zorder=3)
+            if plot_ellipse:
+                plot_cov_ellipse(xc, yc, lambda_, eig_vecs, ax=ax)
+            
+             # format plot   
+            title = "Method " + str(method_num) + " - " + genre
+            ax.set_title(title, fontsize=20)
+            ax.set_xlim(x_lim)
+            ax.set_ylim(y_lim)
+            plt.legend(loc="best", fontsize=16)
+            if save_plots:
+                save_name = "method_%i_genre_%s" % (method_num, genre)
+                if plot_cov:
+                    save_name += "_cov_%i" % (int(1000*var))
+                plt.savefig(save_folder + save_name + ext, bbox_inches="tight")
     
-def title_genres(ind, headers):
+    
+def get_cov_eig(movie_data, data, genre):
+    """
+    Returns eigen values and eigenvectors of covariance matrix
+    """
+    cov_mat = get_cov_mat(movie_data, data, genre)
+    var = np.linalg.det(cov_mat)
+    eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+    x = data[:,0]
+    y = data[:,1]
+    xc = np.mean(x)
+    yc = np.mean(y)
+    lambda_ = np.sqrt(eig_vals)
+    
+    return xc, yc, lambda_, eig_vecs, var
+
+def get_title_genres(ind, headers):
     """
     Returns string of movie title followed by genres.
     """
@@ -190,41 +218,51 @@ def print_most_extreme(movie_data, data_all, num=5):
         sorting_inds_x = np.argsort(data[0,:])
         inds_x_pos = sorting_inds_x[-num:]
         for j in inds_x_pos:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
         
         # smallest negative x value
         print("lowest negative x")
         inds_x_neg = sorting_inds_x[:num]
         for j in inds_x_neg:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
             
         # greatest positive y value
         print("greatest positive y")
         sorting_inds_y = np.argsort(data[1,:])
         inds_y_pos = sorting_inds_y[-num:]
         for j in inds_y_pos:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
             
             
         # lowest negative y value
         print("lowest negative y")
         inds_y_neg = sorting_inds_y[:num]
         for j in inds_y_neg:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
             
         # closest to 0
         print("closest to 0")
         sorting_inds_radii = np.argsort(np.sqrt((data[0,:])**2+(data[1,:])**2))
         inds_near_zero = sorting_inds_radii[:num]
         for j in inds_near_zero:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
             
         # farthest from 0
         print("farthest from 0")
         inds_far_zero = sorting_inds_radii[-num:]
         for j in inds_far_zero:
-            print(title_genres(j, headers))
+            print(get_title_genres(j, headers))
+       
         
+def get_cov_mat(movie_data, data, genre):
+    """
+    Returns covariance matrix of movie data from specific genre.
+    """
+    inds = get_movies_from_genre(movie_data, genre)
+    proj = get_proj(inds, data)
+    
+    return np.cov(np.transpose(proj))
+
         
 def order_genres_by_variance(movie_data, data_all):
     """
@@ -233,34 +271,75 @@ def order_genres_by_variance(movie_data, data_all):
     # get list of genres
     genre_list = list(movie_data)[2:-1]
     # initialize result, list of lists, each list is for one method
-    result = []
+    sorted_genre_lists = []
+    genre_vars = []
     
     # loop through each method
     for data in data_all:
         # array of variances
         var_arr = np.zeros([len(genre_list)])
         for i in range(len(genre_list)):
-            genre = genre_list
-            inds = get_movies_from_genre(movie_data, genre)
-            proj = get_proj(inds, data)
-            (xc, yc) = np.mean(proj, axis=0)
-#            
-#            x = proj[:,0]
-#            y = proj[:,1]
-#            r = np.sqrt((x-xc)**2 + (y-yc)**2)
-#            var_arr[i] = np.mean(r)
-            
-            # how best to estimate variance?
-            pca = PCA(n_components=2)
-            pca.fit(proj)
-            var_arr[i] = pca.singular_values_[0]
+            genre = genre_list[i]
+            # estimate variance with determinant of covariance matrix
+            cov_mat = get_cov_mat(movie_data, data, genre)
+            var_arr[i] = np.linalg.det(cov_mat)
         
         # sort by variance
         inds = np.argsort(var_arr)[::-1]
         sorted_genre_list = [genre_list[ind] for ind in inds]
-        result+= [sorted_genre_list]      
+        sorted_genre_lists += [sorted_genre_list] 
+        genre_vars += [var_arr]
            
-    return result
+    return sorted_genre_lists, genre_vars
+
+
+def plot_cov_ellipse(xc, yc, lambda_, eig_vecs, ax=None, label="", color='k'):
+    """
+    Adds plot of ellipse marking 1 standard deviation to ax.
+    Requires that "data" is passed as a 2 x N array.
+    """
+    ell = Ellipse(xy=(xc, yc), width=lambda_[0]*2, height=lambda_[1]*2,
+                  angle=-np.rad2deg(np.arctan2(eig_vecs[0, 1],eig_vecs[0, 0])), 
+                  edgecolor=color, fc='None', lw=2, label=label)
+    ell.set_facecolor('none')
+    # plot
+    if not ax:
+        ax = plt.subplot(111, aspect='equal')
+    ax.add_artist(ell)
+    ax.add_patch(ell)
+
+
+def plot_genre_ellipses(movie_data, data, genre_list, method_num, 
+                         x_lim=[-1.5,3], y_lim=[-2,3], save_plot=False,
+                         save_folder='figures/', ext='.png'):
+    """
+    Plots covariance matrix ellipses of listed genres on one plot.
+    """
+    cmap = cm.get_cmap("viridis")
+    # initialize figure
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    # plot ellipse for each genre
+    for i in range(len(genre_list)):
+        genre = genre_list[i]
+        # get color factor
+        c = float(i)/(len(genre_list)-1)
+        # plot
+        xc, yc, lambda_, eig_vecs, var = get_cov_eig(movie_data, data, genre)
+        label = "%s, det(cov)=%.3f" % (genre, np.sqrt(var))
+        plot_cov_ellipse(xc, yc, lambda_, eig_vecs, ax=ax, label=label, color=cmap(c))
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
+    ax.set_title("Covariance Ellipses of Genres with Method %i" %(method_num),
+                 fontsize=18)
+    plt.legend(loc="best", fontsize=12)
+    
+    if save_plot:
+        save_name = "method_%i" % method_num
+        for i in range(len(genre_list)):
+            save_name += "_" + genre
+        plt.savefig(save_folder + save_name + ext, bbox_inches="tight")
+
 # main function for running methods
 if __name__=='__main__':
     # load projected data (2 x M and 2 x N)
@@ -268,65 +347,23 @@ if __name__=='__main__':
     
     # load movies and ratings
     movie_data, rating_data = load_data()
-
-    # plot top 10 movies (ratings)
-    inds_best_ten = best_ten_movies(movie_data, rating_data)
-    best_ten_proj_1 = get_proj(inds_best_ten, movies_1)
-    plot_movies(best_ten_proj_1, movie_data, indices = inds_best_ten, title='top 10 - method 1')
     
-    # plot top 10 movies (ratings) - method 2
-    inds_best_ten = best_ten_movies(movie_data, rating_data)
-    best_ten_proj_2 = get_proj(inds_best_ten, movies_2)
-    plot_movies(best_ten_proj_2, movie_data, indices = inds_best_ten, title='top 10 - method 2')
+    data_all = [movies_1, movies_2, movies_3]
+    sorted_genre_lists, genre_vars = order_genres_by_variance(movie_data, data_all)
     
-    # plot top 10 movies (ratings) - method 3
-    best_ten_proj_3 = get_proj(inds_best_ten, movies_3)
-    plot_movies(best_ten_proj_3, movie_data, indices = inds_best_ten, title='top 10 - method 3')
-    
-    # plot most popular 10 movies (popularity)
-    inds_pop_ten = pop_ten_movies(rating_data)
-    pop_ten_proj_1 = get_proj(inds_pop_ten, movies_1)
-    plot_movies(pop_ten_proj_1, movie_data, indices = inds_pop_ten, title='top 10 - most popular')
-
-    movies_all = [movies_1, movies_2, movies_3]
-    # compare genres
-    plot_genres_three_methods(movie_data, movies_all, ["Musical", "Horror",
-                                                       "Documentary", "Western"])
-
-    genre_lists = order_genres_by_variance(movie_data, movies_all)
-    
-    for i in range(len(genre_lists[1])):
-        genre = [genre_lists[1][i]]
-        plot_genres_three_methods(movie_data, [movies_all[1]], genre)
-#    # plot top 10 movies (ratings)
-#    inds_best_ten = best_ten_movies(movie_data, rating_data)
-#    best_ten_proj_1 = get_proj(inds_best_ten, movies_1)
-#    plot_movies(best_ten_proj_1, title='top 10 - method 1')
-#    
-#    # plot top 10 movies (ratings) - method 2
-#    inds_best_ten = best_ten_movies(movie_data, rating_data)
-#    best_ten_proj_2 = get_proj(inds_best_ten, movies_2)
-#    plot_movies(best_ten_proj_2, title='top 10 - method 2')
-#    
-#    # plot top 10 movies (ratings) - method 3
-#    best_ten_proj_3 = get_proj(inds_best_ten, movies_3)
-#    plot_movies(best_ten_proj_3, title='top 10 - method 3')
-#    
-#    # plot most popular 10 movies (popularity)
-#    inds_pop_ten = pop_ten_movies(rating_data)    
-#
-#    pop_ten_proj_1 = get_proj(inds_pop_ten, movies_1)
-#    plot_movies(pop_ten_proj_1)
-#
-#    movies_all = [movies_1, movies_2, movies_3]
-#    # compare genres
-#    plot_genres_three_methods(movie_data, movies_all, ["Musical", "Horror",
-#                                                       "Childrens", "Western",
-#                                                       "Animation"])
-#    
-#    # compare genres
-#    plot_genres_three_methods(movie_data, movies_all, ["Drama", "Comedy",
-#                                                       "Childrens"])
-#    
-#    # print names of movies with most extreme values
-#    print_most_extreme(movie_data, movies_all)
+    for i in range(len(data_all)):
+        data = data_all[i]
+        g = sorted_genre_lists[i]
+        method_num = i+1
+        sparse_and_compact_genres = [g[0], g[1], g[-3], g[-2]]
+        plot_genre_ellipses(movie_data, data, sparse_and_compact_genres,
+                            method_num, save_plot=True)
+        
+    data_all = [users_1, users_2, users_3]
+    for i in range(len(data_all)):
+        data = data_all[i]
+        g = sorted_genre_lists[i]
+        method_num = i+1
+        sparse_and_compact_genres = [g[0], g[1], g[-3], g[-2]]
+        plot_genre_ellipses(movie_data, data, sparse_and_compact_genres,
+                            method_num, save_plot=True)
